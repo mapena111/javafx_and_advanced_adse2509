8da57bc87f2a30d81729ba62e6c36fb5f83884d4
mapena111 <olomomoses75@gmail.com>
2026-08-31 13:38:23 +0300

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.javafx_and_advanced_adse2509;
+
+/**
+ *
+ * @author Administrator
+ */
+public class Javafx_and_advanced_adse2509 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
